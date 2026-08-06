69234c1a5d3ddbda750a4711f835d4023e79b13e
Mohammed Naji <m.naji1512@gmail.com>
2026-08-06 13:46:56 +0300

    route part 1

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return 'Homepage';
+// });
+
+// home, about, team, services
+
+// Route::get('/', function () {
+//     return 'Homepage';
+// });
+
+// Route::get('post', function () {
+//     return 'About Us';
+// });
+
+// Route::get('/{id}', function ($id) {
+//     return "Not Found";
+// });
+
+// Route::match(['put', 'patch', 'post'], '/', function () {});
+
+
+// Route::fallback(function () {
+//     // return 'هادي الصفحة مش موجودة اخوي بعينك الله دور على رابط تاني';
+//     return redirect('/');
+// });
+
+// Route::get('/', function () {
+//     // url generation
+//     // return '<a href="/about">About Us</a>';
+//     // $link = url('/about');
+//     // $link = url('/about');
+//     $link = route('abc');
+//     return "<a href='$link'>About Us</a>";
+// });
+
+// Route::get('/about-meeeeeee', function () {
+//     return 'About Us Page Goes Here';
+// })->name('abc');
